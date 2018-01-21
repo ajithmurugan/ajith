72a444f8af6fef8093507f42b75b5bd8c33a9aa3
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-01-21 04:20:01 +0000

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+int i,c,b ,e,sum;
+sum=0;
+printf("enter the number");
+scanf("%d",&c);
+for(i=c;i>1;i++)
+{
+sum=sum+(b+(c-1)*e);
+}
+sum=sum+b;
+printf("%d",sum);
+return 0;
+}
