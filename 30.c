0e5f5d44546fb9970bf508050ec79e56d981a2dd
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-01-21 06:33:19 +0000

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int g,h,i,j,k,l;
+printf("enter the first timein hours and minutes");
+scanf("%d%d",&g,&i);
+printf("enter the second time in hours and minutes");
+scanf("%d%d",&h,&j);
+k=g-h;
+l=i-j;
+printf("the difference is %d%d",&k,&l);
+}
