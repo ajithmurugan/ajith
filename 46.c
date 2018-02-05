e9c2808b8e4b834c42c7163fe81e2bf8767b31d4
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-02-05 16:16:07 +0530

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a;
+printf("enter any number");
+scanf("%d",&a);
+printf("natural number from 1 to n:\n");
+for(i=1;i<=a;i++)
+{
+printf("%d",i);
+}
+getch();
+}
