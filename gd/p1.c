bcf534f8cfcd49a695cfe8bc0c05491130e7c124
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-02-05 16:26:09 +0530

    Rename 1.c to p1.c

---
+++ gd/p1.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+ int t;
+ printf("enter the number\n");
+ scanf("%d",&t);
+ if(t<0)
+ printf("number is negative",t);
+ if(t==0)
+ printf("number is zero",t);
+ if(t>0)
+ printf("number is positive",t);
+ return 0;
+ }
