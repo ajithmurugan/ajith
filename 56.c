b24ab8b296df0e0eb1dd56a9cbe65ddc8381cea8
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-02-09 16:50:59 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    char s[100];
+    scanf("%c",&s[i]);
+    if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z'))
+    
+        printf("\n given is alphabet \n");
+    
+    else
+    
+        printf("\n given is number \n");
+    
+    
+getch();
+}
