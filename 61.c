34181f30369bcf1241539258b45e42decdc0b48e
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-02-19 16:05:12 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+ void
+main () 
+{
+  
+char s[10];
+  
+scanf ("%s", s);
+  
+int n, i;
+  
+scanf ("%d", &n);
+  
+for (i = 0; i < n; i++)
+    
+    {
+      
+printf ("enter the chr %c \t", s[i]);
+    
+}
+  
+getch ();
+
+}
