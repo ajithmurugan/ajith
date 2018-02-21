ed4ec4de9b9607284384468567775ebe2dbb8cf4
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-02-21 16:52:40 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+	int s,i,count=0,n;
+	printf("enter the value of s \t");
+	scanf("%d",&s);
+	printf("enter the value of n");
+	scanf("%d",&n);
+	if(count==2)
+	printf("%d is the prime number",s);
+	else
+	printf("%d is not a prime number",s);
+	return 0;
+	
+}
