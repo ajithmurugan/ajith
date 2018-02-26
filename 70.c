bf2794ed9b6d98f965427455e350c780c1406f07
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-02-26 16:34:58 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+    int  n;
+    printf("enter the value");
+    scanf("%d",&n);
+    n=n*2;
+    printf("the power of given nearest  value is: %d",n);
+    getch();
+}
+    
