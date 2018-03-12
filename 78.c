73da6e06297c26708d0a747d98277bc6d50f08c4
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-03-12 16:12:10 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,21 @@
+#include<conio.h>
+#include<stdio.h>
+void
+main ()
+{
+  int s;
+  printf ("enter the value \n");
+  scanf ("%d", &s);
+  
+    
+      if (s % 13 == 0)
+	{
+	  printf ("yes");
+	}
+	else
+	{
+	    printf("no");
+	}
+    
+  getch ();
+}
