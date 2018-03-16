359502e89b8ecb9776f5751b3a9aa6c8571d625c
ajithmurugan <35128572+ajithmurugan@users.noreply.github.com>
2018-03-16 16:47:51 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include<math.h>
+void
+main () 
+{
+  
+float a,b,n;
+printf("enter the values:\n");
+scanf("%f %f",&a,&b);
+n=a*b;
+printf("%2f \t",n);
+
+getch();
+}
+  
+
+
+
+ 
+ 
+ 
